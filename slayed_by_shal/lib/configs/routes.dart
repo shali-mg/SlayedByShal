493b9a2a3a161516e757018d66241e02b5ddc6c0
Shali <gichurishalon@gmail.com>
2026-07-19 00:42:29 +0300

    First routes

--- slayed_by_shal/lib/configs/routes.dart
+++ slayed_by_shal/lib/configs/routes.dart
@@ -1,1 +1,10 @@
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:slayed_by_shal/views/home.dart';
+import 'package:slayed_by_shal/views/login.dart';
+import 'package:slayed_by_shal/views/registration.dart';
 
+var routes = [
+  GetPage(name: "/", page: () => LoginScreen()),
+  GetPage(name: "/register", page: () => RegistrationScreen()),
+  GetPage(name: "/home", page: () => HomeScreen()),
+];
