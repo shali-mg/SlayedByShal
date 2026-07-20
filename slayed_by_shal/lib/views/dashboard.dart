1ad3b6f57a18c85b91391c44f5fd9897801db56b
Shali <gichurishalon@gmail.com>
2026-07-21 00:24:18 +0300

    Found a better tutorial https://www.youtube.com/watch?v=YEJPg2jwzI8&t=144s \ And the source code is in the description https://github.com/abuanwar072/Flutter-Shop-UI-Kit \ tweaking it to suit my app

--- slayed_by_shal/lib/views/dashboard.dart
+++ slayed_by_shal/lib/views/dashboard.dart
@@ -1,51 +1,62 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:slayed_by_shal/configs/colors.dart';
-import 'package:slayed_by_shal/widgets/containers.dart';
-import 'package:slayed_by_shal/widgets/curvededges.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
 
   @override
-  State<DashboardScreen> createState() => _DashboardPageState();
+  State<DashboardScreen> createState() => _DashboardScreenState();
 }
 
-class _DashboardPageState extends State<DashboardScreen> {
+class _DashboardScreenState extends State<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
+    return SizedBox(
+      height: 84,
+      child: ListView.separated(
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (context, index) => CategoryCard(
+          icon: categories[index].icon,
+          title: categories[index].title,
+          press: () {},
+        ),
+        separatorBuilder: (context, index) => const SizedBox(width: 15),
+      ),
+    );
+  }
+}
+
+class CategoryCard extends StatelessWidget {
+  const CategoryCard({
+    Key? key,
+    required this.icon,
+    required this.title,
+    required this.press,
+  }) : super(key: key);
+
+  final String icon, title;
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      onPressed: press,
+      style: OutlinedButton.styleFrom(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(defaultBorderRadius)),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: defaultPadding / 2,
+          horizontal: defaultPadding / 4,
+        ),
         child: Column(
           children: [
-            ClipPath(
-              clipper: Curvededges(),
-              child: Container(
-                color: primaryColor,
-                padding: const EdgeInsets.all(0),
-                child: SizedBox(
-                  height: 400,
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        top: -150,
-                        right: -250,
-                        child: CircularContainer(
-                          backgroundColor: Colors.white.withOpacity(0.1),
-                        ),
-                      ),
-                      Positioned(
-                        top: 100,
-                        right: -300,
-                        child: CircularContainer(
-                          backgroundColor: Colors.white.withOpacity(0.1),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
+            SvgPicture.asset(icon),
+            const SizedBox(height: defaultPadding / 2),
+            Text(title, style: Theme.of(context).textTheme.subtitle2),
           ],
         ),
       ),
